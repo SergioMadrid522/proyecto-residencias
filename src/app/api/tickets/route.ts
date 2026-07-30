a83ddaabf88ea2bee333140502cee4eebe7aa36f
SergioMadrid522 <sergiofabian.madrid@hotmail.com>
2026-07-30 16:37:55 -0600

    fix create ticket endpoint

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -38,32 +38,41 @@ export async function POST(request: Request) {
     if (!validation.success) {
       return NextResponse.json({ error: validation.errors }, { status: 400 });
     }
+    const hasManualSeverity = validation.data.severidadIa !== null;
 
-    const prompt = {
-      titulo: validation.data?.titulo,
-      descripcion: validation.data?.descripcion,
-      pasosReproducir: validation.data?.pasosReproducir ?? "No hay alguno",
-      modulo: validation.data?.modulo,
-    };
-
-    const { severidadIa, ...ticketData } = validation.data;
-    const genAI = await validatePrompt(prompt);
-
-    if (!genAI.success) {
-      return NextResponse.json(
-        {
-          error: genAI.error,
+    if (hasManualSeverity) {
+      await prisma.ticket.create({
+        data: {
+          ...validation.data,
         },
-        { status: genAI.status },
-      );
-    }
+      });
+    } else {
+      const prompt = {
+        titulo: validation.data?.titulo,
+        descripcion: validation.data?.descripcion,
+        pasosReproducir: validation.data?.pasosReproducir ?? "No hay alguno",
+        modulo: validation.data?.modulo,
+      };
 
-    await prisma.ticket.create({
-      data: {
-        ...ticketData,
-        severidadIa: genAI.output,
-      },
-    });
+      const { severidadIa, ...ticketData } = validation.data;
+      const genAI = await validatePrompt(prompt);
+
+      if (!genAI.success) {
+        return NextResponse.json(
+          {
+            error: genAI.error,
+          },
+          { status: genAI.status },
+        );
+      }
+
+      await prisma.ticket.create({
+        data: {
+          ...ticketData,
+          severidadIa: genAI.output,
+        },
+      });
+    }
 
     return NextResponse.json(
       { message: "El ticket se ha creado con éxito." },
